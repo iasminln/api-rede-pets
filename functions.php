1070ef13ffdd2cd375332d0c15b2ca5450898335
Iasmin <iasln@outlook.com>
2023-08-21 17:43:06 -0300

    first commit!

---
+++ functions.php
@@ -0,0 +1,38 @@
+<?php
+// remove_action('rest_api_init', 'create_initial_rest_routes', 99);
+
+add_filter('rest_endpoints', function ($endpoints) {
+  unset($endpoints['/wp/v2/users']);
+  unset($endpoints['/wp/v2/users/(?P<id>[\d]+)']);
+  return $endpoints;
+});
+
+$dirbase = get_template_directory();
+require_once $dirbase . '/endpoints/user_post.php';
+require_once $dirbase . '/endpoints/user_get.php';
+
+require_once $dirbase . '/endpoints/photo_post.php';
+require_once $dirbase . '/endpoints/photo_delete.php';
+require_once $dirbase . '/endpoints/photo_get.php';
+
+require_once $dirbase . '/endpoints/comment_post.php';
+require_once $dirbase . '/endpoints/comment_get.php';
+
+require_once $dirbase . '/endpoints/password.php';
+
+require_once $dirbase . '/endpoints/stats_get.php';
+
+// Forçar o tamanho e corte da foto
+update_option('large-size_w', 1000);
+update_option('large-size_h', 1000);
+update_option('large-crop', 1);
+
+function expire_token()
+{
+  return time() + (60 * 60 * 24);
+}
+;
+
+add_action('jwt_auth_expires', 'expire_token');
+
+?>
